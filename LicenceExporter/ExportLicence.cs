be533aa49cbfa60c501bcf743f92e0ea79f72434
FR_DEV <Richard.Finszter@multisoft.hu>
2022-04-01 15:52:36 +0200

    Init

---
+++ LicenceExporter/ExportLicence.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace LicenceExporter
+{
+    public class ExportLicence
+    {
+        public static void Export(string SQLServer, string UserName, string Password, string Database, SQLAuthenticationTypes ConnectionType)
+        {
+            Console.WriteLine("Export start");
+
+            try
+            {
+                string table;
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+                {
+                    DataSource = SQLServer
+                };
+                if (string.IsNullOrEmpty(Database))
+                {
+                    builder.InitialCatalog = "master";
+                    table = "[$ndo$srvproperty]";
+                }
+                else
+                {
+                    builder.UserID = UserName;
+                    builder.Password = Password;
+                    builder.InitialCatalog = Database;
+                    table = "[$ndo$dbproperty]";
+                }
+                if (ConnectionType == SQLAuthenticationTypes.Windows)
+                    builder.IntegratedSecurity = true;
+
+                Console.WriteLine(builder.ConnectionString);
+                SqlConnection Con = new SqlConnection(builder.ConnectionString);
+                Con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT license from " + table, Con);
+                SqlDataReader data = cmd.ExecuteReader();
+                while (data.Read())
+                {
+                    MemoryStream ms = new MemoryStream();
+                    data.GetStream(0).CopyTo(ms);
+                    //SaveFileDialog sfd = new SaveFileDialog();
+                    //sfd.Filter = "License|*.flf";
+                    //if (sfd.ShowDialog() == true)
+                    //{
+                    FileStream fs = new FileStream("filename.flf", FileMode.CreateNew);
+                    ms.Position = 0;
+                    ms.CopyTo(fs);
+                    fs.Close();
+                    Console.WriteLine("Succes, exported as " + "filename.flf");
+                    //}
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ LicenceExporter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Management.Automation;
+//DLL
+
+namespace LicenceExporter
+{
+    // Declare the class as a cmdlet and specify the
+    // appropriate verb and noun for the cmdlet name.
+    [Cmdlet(VerbsCommunications.Send, "LicenceExporter")]
+    public class LicenceExporter : Cmdlet
+    {
+        // Declare the parameters for the cmdlet.
+        [Parameter(Mandatory = true)]
+        public string SQLServer { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string SQLDatabase { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string SQLUserName { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string SQLPassword { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public SQLAuthenticationTypes SQLAuthenticationType { get; set; }
+
+        // Override the ProcessRecord method to process
+        // the supplied user name and write out a
+        // greeting to the user by calling the WriteObject
+        // method.
+        protected override void ProcessRecord()
+        {
+            WriteObject("ProcessRecord: " + SQLServer + " " + SQLDatabase + " " + SQLUserName + " " + SQLPassword + " " + SQLAuthenticationType.ToString());
+            //TODO: Error: The file 'C:\Users\finszterr\filename.flf' already exists.
+            //**************
+            ExportLicence.Export(SQLServer, SQLUserName, SQLPassword, SQLDatabase, SQLAuthenticationType);
+        }
+    }
+}
